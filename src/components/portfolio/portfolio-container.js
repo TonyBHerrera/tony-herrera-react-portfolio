c3588bcc859ad1f429ada000d1eaa4c8ec878ef9
TonyBHerrera <tonybherrera@yahoo.com>
2020-04-02 10:01:21 -0600

    Overview of Props in React

--- src/components/portfolio/portfolio-container.js
+++ src/components/portfolio/portfolio-container.js
@@ -13,7 +13,7 @@ export default class PortfolioContainer extends Component {
         const data = ["Quip", "Eventbright", "Ministry Safe", "Swing Away"];
 
         return data.map(item => {
-            return <PortfolioItem />
+            return <PortfolioItem title={item} />
         })
     }
 
