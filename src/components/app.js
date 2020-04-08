c0d799de4dc026a573accd7a6e3d101fcac77ce0
TonyBHerrera <tonybherrera@yahoo.com>
2020-04-08 15:36:59 -0600

    Implementing a Catch All Route with React Router

--- src/components/app.js
+++ src/components/app.js
@@ -13,6 +13,7 @@ import Home from './pages/home'
 import Contact from './pages/contact'
 import Blog from './pages/blog'
 import PortfolioDetail from './portfolio/portfolioDetail'
+import NoMatch from './pages/no-match'
 
 export default class App extends Component {
   render() {
@@ -31,7 +32,8 @@ export default class App extends Component {
               <Route path="/about-me" component={About} />
               <Route path="/contact" component={Contact} />
               <Route path="/blog" component={Blog} />
-              <Route path="/portfolio/:slug" component={PortfolioDetail} />
+              <Route exact path="/portfolio/:slug" component={PortfolioDetail} />
+              <Route component={NoMatch} />
             </Switch>
           </div>
         </Router>
