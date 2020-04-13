9d18dfe9034609b4f1e36f369e2c23d2d800a171
TonyBHerrera <tonybherrera@yahoo.com>
2020-04-13 14:20:11 -0600

    Building the Style Structure for the Homepage Portfolio Items

--- src/components/portfolio/portfolio-container.js
+++ src/components/portfolio/portfolio-container.js
@@ -73,7 +73,8 @@ export default class PortfolioContainer extends Component {
                     Enterprise
                 </button>
 
-                {this.portfolioItems()}
+                <div className="portfolio-items-wrapper">{this.portfolioItems()}</div>
+
 
             </div>
         )
