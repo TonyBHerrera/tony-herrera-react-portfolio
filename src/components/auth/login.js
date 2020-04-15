059cee6517825aa6cb8cc829f48b021579c27a18
TonyBHerrera <tonybherrera@yahoo.com>
2020-04-15 13:34:49 -0600

    adding functional sign in page

--- src/components/auth/login.js
+++ src/components/auth/login.js
@@ -39,16 +39,19 @@ export default class Login extends Component {
             .then(response => {
                 if (response.data.status === 'created') {
                     console.log("you can come in..")
+                    this.props.handleSuccessfulAuth()
                 } else {
                     this.setState({
                         errorText: "Wrong email or password"
                     })
+                    this.props.handleUnSuccessfulAuth
                 }
             }).catch(error => {
                 console.log("some error occured", error)
                 this.setState({
                     errorText: "An error occured"
                 })
+                this.props.handleUnSuccessfulAuth()
             })
 
         event.preventDefault()
