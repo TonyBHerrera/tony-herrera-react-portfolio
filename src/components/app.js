8c97e808db36e6b9274d9d47a310ae87e5d5f9e4
TonyBHerrera <tonybherrera@yahoo.com>
2020-04-15 15:04:34 -0600

    refactoring navigation component

--- src/components/app.js
+++ src/components/app.js
@@ -4,6 +4,7 @@ import {
   Switch,
   Route
 } from 'react-router-dom'
+import axios from "axios"
 
 import PortfolioContainer from './portfolio/portfolio-container'
 import NavigationContainer from './navigation/navigation-container'
@@ -14,6 +15,7 @@ import Blog from './pages/blog'
 import PortfolioDetail from './portfolio/portfolioDetail'
 import Auth from './pages/auth'
 import NoMatch from './pages/no-match'
+
 
 export default class App extends Component {
   constructor(props) {
@@ -36,6 +38,37 @@ export default class App extends Component {
     this.setState({
       loggedInStatus: "NOT_LOGGED_IN"
     })
+  }
+
+  checkLoginStatus() {
+    return axios.get("https://api.devcamp.space/logged_in", {
+      withCredentials: true
+    }).then(response => {
+      const loggedIn = response.data.logged_in
+      const loggedInStatus = this.state.loggedInStatus
+
+      // If loggedIn and status LOGGED_IN => return data
+      // If loggedIn status NOT_LOGGED_IN => update state
+      // If not loggedIn and status LOGGED_IN => update state
+
+      if (loggedIn && loggedInStatus === "LOGGED_IN") {
+        return loggedIn
+      } else if (loggedIn && loggedInStatus === "NOT_LOGGED_IN") {
+        this.setState({
+          loggedInStatus: "LOGGED_IN"
+        })
+      } else if (!loggedIn && loggedInStatus === "LOGGED_IN") {
+        this.setState({
+          loggedInStatus: "NOT_LOGGED_IN"
+        })
+      }
+    })
+      .catch(error => {
+        console.log("Error", error)
+      })
+  }
+  componentDidMount() {
+    this.checkLoginStatus()
   }
 
   render() {
--- src/components/navigation/navigation-container.js
+++ src/components/navigation/navigation-container.js
@@ -1,47 +1,41 @@
-import React, { Component } from 'react'
+import React from 'react'
 import { NavLink } from 'react-router-dom'
 
-export default class NavigationComponent extends Component {
-    constructor() {
-        super()
-    }
+const NavigationComponent = (props) => {
 
-
-
-    render() {
-        return (
-            <div className="nav-wrapper">
-                <div className="left-side">
-                    <div className="nav-link-wrapper">
-                        <NavLink exact to="/">
-                            Home
-                        </NavLink>
-                    </div>
-
-                    <div className="nav-link-wrapper">
-                        <NavLink to="/about-me" activeClassName="nav-link-active">
-                            About
-                        </NavLink>
-                    </div>
-
-                    <div className="nav-link-wrapper">
-                        <NavLink to="/contact" activeClassName="nav-link-active">
-                            Contact
-                        </NavLink>
-                    </div>
-
-                    <div className="nav-link-wrapper">
-                        <NavLink to="/blog" activeClassName="nav-link-active">
-                            Blog
-                        </NavLink>
-                    </div>
-                    {false ? <button>Add Blog</button> : null}
+    return (
+        <div className="nav-wrapper">
+            <div className="left-side">
+                <div className="nav-link-wrapper">
+                    <NavLink exact to="/">
+                        Home
+                    </NavLink>
                 </div>
-                <div className="right-side">Tony Herrera</div>
+
+                <div className="nav-link-wrapper">
+                    <NavLink to="/about-me" activeClassName="nav-link-active">
+                        About
+                    </NavLink>
+                </div>
+
+                <div className="nav-link-wrapper">
+                    <NavLink to="/contact" activeClassName="nav-link-active">
+                        Contact
+                    </NavLink>
+                </div>
+
+                <div className="nav-link-wrapper">
+                    <NavLink to="/blog" activeClassName="nav-link-active">
+                        Blog
+                    </NavLink>
+                </div>
+                {false ? <button>Add Blog</button> : null}
             </div>
-        )
+            <div className="right-side">Tony Herrera</div>
+        </div>
+    )
 
-
-    }
 
 }
+
+export default NavigationComponent    
