8e2772bd4f57564dd0bf22d3b23ec9fc020385c4
TonyBHerrera <tonybherrera@yahoo.com>
2020-04-24 15:08:22 -0600

    adding styles to blog

--- src/components/pages/blog-detail.js
+++ src/components/pages/blog-detail.js
@@ -38,11 +38,14 @@ export default class BlogDetail extends Component {
         } = this.state.blogItem
 
         return (
-            <div>
-                <h1>{title}</h1>
-                <img src={featured_image_url} />
-                <div>
-                    {content}
+            <div className="blog-container">
+                <div className="content-container">
+                    <h1>{title}</h1>
+
+                    <div className="feature-image-wrapper">
+                        <img src={featured_image_url} />
+                    </div>
+                    <div className="content">{content}</div>
                 </div>
             </div>
         )
