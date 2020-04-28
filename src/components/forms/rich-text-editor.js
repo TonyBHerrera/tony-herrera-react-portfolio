5da1746321178ee617af6c9c286776edd602e489
TonyBHerrera <tonybherrera@yahoo.com>
2020-04-28 16:13:59 -0600

    Positioning Draft JS Icons

--- src/components/forms/rich-text-editor.js
+++ src/components/forms/rich-text-editor.js
@@ -12,6 +12,9 @@ export default class RichTextEditor extends Component {
             editorState: EditorState.createEmpty()
         }
         this.onEditorStateChange = this.onEditorStateChange.bind(this)
+        this.getBase64 = this.getBase64.bind(this)
+        this.uploadFile = this.uploadFile.bind(this)
+
     }
 
     onEditorStateChange(editorState) {
@@ -22,6 +25,19 @@ export default class RichTextEditor extends Component {
             )
         )
     }
+    getBase64(file, callBack) {
+        let reader = new FileReader()
+        reader.readAsDataURL(file)
+        reader.onload = () => callBack(reader.result)
+        reader.onerror = error => { };
+
+    }
+
+    uploadFile(file) {
+        return new Promise((resolve, reject) => {
+            this.getBase64(file, data => resolve({ data: { link: data } }))
+        })
+    }
 
     render() {
         return (
@@ -31,6 +47,19 @@ export default class RichTextEditor extends Component {
                     wrapperClassName="demo-wrapper"
                     editorClassName="demo-editor"
                     onEditorStateChange={this.onEditorStateChange}
+                    toolbar={{
+                        inline: { inDropdown: true },
+                        list: { inDropdown: true },
+                        textAlign: { inDropdown: true },
+                        link: { inDropdown: true },
+                        history: { inDropdown: true },
+                        image: {
+                            uploadCallback: this.uploadFile,
+                            alt: { present: true, mandatory: false },
+                            previewImage: true,
+                            inputAccept: "image/gif,image/jpeg,image/jpg,image/png,image/svg"
+                        }
+                    }}
                 />
             </div>
         )
