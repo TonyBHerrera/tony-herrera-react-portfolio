50bca1dca6e6880437f4b4aacaa12234ae9149cd
TonyBHerrera <tonybherrera@yahoo.com>
2020-04-28 16:50:17 -0600

    How to Programmatically Add Content to Draft JS

--- src/components/blog/blog-form.js
+++ src/components/blog/blog-form.js
@@ -10,6 +10,7 @@ export default class BlogForm extends Component {
         super(props)
 
         this.state = {
+            id: "",
             title: "",
             blog_status: "",
             content: "",
@@ -27,6 +28,16 @@ export default class BlogForm extends Component {
 
         this.featuredImageRef = React.createRef();
 
+    }
+
+    componentWillMount() {
+        if (this.props.editMode) {
+            this.setState({
+                id: this.props.blog.id,
+                title: this.props.blog.title,
+                status: this.props.blog.status
+            })
+        }
     }
 
     componentConfig() {
@@ -128,7 +139,10 @@ export default class BlogForm extends Component {
 
                 <div className="one-column">
                     <RichTextEditor
-                        handleRichTextEditorChange={this.handleRichTextEditorChange} />
+                        handleRichTextEditorChange={this.handleRichTextEditorChange}
+                        editMode={this.props.editMode}
+                        contentToEdit={this.props.editMode && this.props.blog.content ? this.props.blog.content : null}
+                    />
                 </div>
 
                 <div className="image-uploaders">
