1bfd2d92db7fb53471449d8483ae1fc7151441c5
TonyBHerrera <tonybherrera@yahoo.com>
2020-04-29 15:58:29 -0600

    Building Contact Page Styles for the React Portfolio

--- src/components/pages/about.js
+++ src/components/pages/about.js
@@ -1,9 +1,26 @@
 import React from 'react'
+import profilePicture from "../../../static/assets/images/bio/scarecrow.jpg"
 
 export default function () {
     return (
-        <div>
-            About Me
+        <div className="content-page-wrapper">
+            <div className="left-column"
+                style={{
+                    background: "url(" + profilePicture + ") no-repeat",
+                    backgroundSize: "cover",
+                    backgroundPosition: "center"
+                }}
+            />
+
+
+            <div className="right-column">
+                The test is not a complex one: when the alarm goes off,
+                do you get up out of bed, or do you lie there in comfort and fall back to sleep?
+                If you have the discipline to get out of bed, you win—you pass the test.
+                If you are mentally weak for that moment and you let that weakness keep you in bed, you fail.
+                Though it seems small, that weakness translates to more significant decisions.
+                But if you exercise discipline, that too translates to more substantial elements of your life.
+            </div>
         </div>
     )
 }
--- src/components/pages/contact.js
+++ src/components/pages/contact.js
@@ -1,9 +1,53 @@
 import React from 'react'
+import contactPagePicture from "../../../static/assets/images/auth/login.jpg"
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+
 
 export default function () {
     return (
-        <div>
-            Contact
+        <div className="content-page-wrapper">
+            <div className="left-column"
+                style={{
+                    background: "url(" + contactPagePicture + ") no-repeat",
+                    backgroundSize: "cover",
+                    backgroundPosition: "center"
+                }}
+            />
+
+
+            <div className="right-column">
+
+                <div className="contact-bullet-points">
+                    <div className="bullet-point-group">
+                        <div className="icon">
+                            <FontAwesomeIcon icon="phone" />
+                        </div>
+                        <div className="text">
+                            801-989-6229
+                        </div>
+                    </div>
+                    <div className="bullet-point-group">
+                        <div className="icon">
+                            <FontAwesomeIcon icon="envelope" />
+                        </div>
+                        <div className="text">
+                            tonybherrera@gmail.com
+                        </div>
+                    </div>
+                    <div className="bullet-point-group">
+                        <div className="icon">
+                            <FontAwesomeIcon icon="map-marker-alt" />
+                        </div>
+                        <div className="text">
+                            Layton,UT
+                        </div>
+                    </div>
+
+
+
+
+                </div>
+            </div>
         </div>
     )
 }
