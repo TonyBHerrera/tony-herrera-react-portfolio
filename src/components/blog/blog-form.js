ec5a7f8c2717ae60af650250808ed0d61dc8798f
TonyBHerrera <tonybherrera@yahoo.com>
2020-04-29 14:46:25 -0600

    Enabling Authorization Rules for Blog Detail Component

--- src/components/blog/blog-form.js
+++ src/components/blog/blog-form.js
@@ -14,7 +14,9 @@ export default class BlogForm extends Component {
             title: "",
             blog_status: "",
             content: "",
-            featured_image: ""
+            featured_image: "",
+            apiUrl: "https://tonybherrera.devcamp.space/portfolio/portfolio_blogs",
+            apiAction: "post"
 
         }
 
@@ -53,7 +55,10 @@ export default class BlogForm extends Component {
             this.setState({
                 id: this.props.blog.id,
                 title: this.props.blog.title,
-                status: this.props.blog.status
+                blog_status: this.props.blog.blog_status,
+                content: this.props.blog.content,
+                apiUrl: `https://tonybherrera.devcamp.space/portfolio/portfolio_blogs/${this.props.blog.id}`,
+                apiAction: "patch"
             })
         }
     }
@@ -99,13 +104,12 @@ export default class BlogForm extends Component {
     }
 
     handleSubmit(event) {
-        axios
-            .post(
-                "https://tonybherrera.devcamp.space/portfolio/portfolio_blogs",
-                this.buildForm(),
-                { withCredentials: true }
-            )
-
+        axios({
+            method: this.state.apiAction,
+            url: this.state.apiUrl,
+            data: this.buildForm(),
+            withCredentials: true
+        })
             .then(response => {
                 if (this.state.featured_image) {
                     this.featuredImageRef.current.dropzone.removeAllFiles();
@@ -117,10 +121,14 @@ export default class BlogForm extends Component {
                     featured_image: ""
                 })
 
-
-                this.props.handleSuccessfulFormSubmission(
-                    response.data.portfolio_blog
-                )
+                if (this.props.editMode) {
+                    // update blog detail
+                    this.props.handleUpdateFormSubmission(response.data.portfolio_blog)
+                } else {
+                    this.props.handleSuccessfulFormSubmission(
+                        response.data.portfolio_blog
+                    )
+                }
             })
             .catch(error => {
                 console.log("handleSubmit for blog error", error)
